6ea8d6b3fe10a3326e1634500e64203f629316cb
Andrei Dyminski <smartfox87@gmail.com>
2024-04-04 16:37:21 +0200

    logo

--- src/components/Layout/Header/Logo.jsx
+++ src/components/Layout/Header/Logo.jsx
@@ -8,7 +8,7 @@ export const Logo = () => {
   return (
     <Link href="/" title={t("buttons.go_home")} className="-ml-1.5 -mt-2 flex items-center gap-2 text-gray-700 duration-300 hover:text-gray-900 dark:text-white/70 dark:hover:text-white/90">
       <SvgLogo className="h-10 w-10" />
-      <div className="text-2xl font-bold">My Finance</div>
+      <div className="text-2xl font-bold">{t("seo.app_name")}</div>
     </Link>
   );
 };
