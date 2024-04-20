60207938b1126f98f8eb4d07ce2dabf0924a9e3e
Andrei Dyminski <smartfox87@gmail.com>
2024-04-20 12:23:05 +0200

    Revert "disable sentry for development"
    This reverts commit 5359f5ef1fc1a3083c7f1628948755304ce47d9d.

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -5,14 +5,12 @@
 
 import * as Sentry from "@sentry/nextjs";
 
-if (process.env.NODE_ENV === "production") {
-  Sentry.init({
-    dsn: "https://50c8b3705c7c2000afa094ba5cfdd108@o4507073354989568.ingest.de.sentry.io/4507073364820048",
+Sentry.init({
+  dsn: "https://50c8b3705c7c2000afa094ba5cfdd108@o4507073354989568.ingest.de.sentry.io/4507073364820048",
 
-    // Adjust this value in production, or use tracesSampler for greater control
-    tracesSampleRate: 1,
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
 
-    // Setting this option to true will print useful information to the console while you're setting up Sentry.
-    debug: false,
-  });
-}
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
