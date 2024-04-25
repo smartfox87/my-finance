2a4c49d181bca11f12245ea9456915b07ca1dc33
Andrei Dyminski <smartfox87@gmail.com>
2024-04-25 18:22:33 +0200

    accounts slice typescript

--- src/hooks/redux.ts
+++ src/hooks/redux.ts
@@ -1,4 +1,7 @@
-import { useDispatch } from "react-redux";
-import { AppDispatch } from "@/types/redux";
+import { useDispatch, useSelector } from "react-redux";
+import type { TypedUseSelectorHook } from "react-redux";
+import { AppDispatch, RootState } from "@/types/redux";
 
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+// export const useAppStore: () => AppStore = useStore
