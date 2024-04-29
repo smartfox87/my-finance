eb8e924985f1a176e0f025283822dc3899d84179
Andrei Dyminski <smartfox87@gmail.com>
2024-04-29 21:29:29 +0200

    test without actions

--- src/app/[locale]/(inner)/layout.tsx
+++ src/app/[locale]/(inner)/layout.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Header } from "@/components/Layout/Header/Header.jsx";
+// import { Header } from "@/components/Layout/Header/Header.jsx";
 import { MainNav } from "@/components/Layout/MainNav";
 import { useViewport } from "@/hooks/viewport.js";
 import { useEffect } from "react";
@@ -8,7 +8,7 @@ import { useSelector } from "react-redux";
 import { selectUser } from "@/store/selectors/auth.js";
 import { useTranslation } from "react-i18next";
 import { getUserId } from "@/helpers/localStorage.js";
-import { MobileNav } from "@/components/Layout/MobileNav";
+// import { MobileNav } from "@/components/Layout/MobileNav";
 import { ReactNodeLike } from "prop-types";
 // import { getAccountsListThunk } from "@/store/accountsSlice";
 import { useAppDispatch } from "@/hooks/redux";
@@ -48,12 +48,12 @@ export default function MainLayout({ children }: { children: ReactNodeLike }) {
 
   return (
     <>
-      <Header />
+      {/*<Header />*/}
       <main className="container flex grow">
         <MainNav className="hidden lg:block" />
         <div className="relative flex min-w-0 grow flex-col py-4 md:py-6 lg:ml-6">{children}</div>
       </main>
-      <div className="sticky bottom-0 z-30 bg-white dark:bg-dark">{["sm", "xs", "xxs"].includes(viewport) && <MobileNav />}</div>
+      {/*<div className="sticky bottom-0 z-30 bg-white dark:bg-dark">{["sm", "xs", "xxs"].includes(viewport) && <MobileNav />}</div>*/}
     </>
   );
 }
