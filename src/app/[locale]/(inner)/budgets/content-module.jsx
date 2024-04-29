4efff03877543f1e6d72cf9e68d312b5552e35fa
Andrei Dyminski <smartfox87@gmail.com>
2024-04-29 21:20:00 +0200

    delete pages

--- src/app/[locale]/(inner)/budgets/content-module.jsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import dynamic from "next/dynamic";
-
-const BudgetsContent = dynamic(() => import("@/app/[locale]/(inner)/budgets/content"));
-
-export default function BudgetsModule() {
-  return <BudgetsContent />;
-}
--- src/app/[locale]/(inner)/contact/content-module.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import dynamic from "next/dynamic";
-
-const ContactContent = dynamic(() => import("@/app/[locale]/(inner)/contact/content"));
-
-export default function ContactModule() {
-  return <ContactContent />;
-}
