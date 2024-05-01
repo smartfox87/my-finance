94d6eec291729a272c51504ef32e4bf7b6ad296f
Andrei Dyminski <smartfox87@gmail.com>
2024-05-01 20:27:50 +0200

    fix

--- src/app/[locale]/(inner)/content.tsx
+++ src/app/[locale]/(inner)/content.tsx
@@ -2,7 +2,7 @@
 
 import { selectProfile } from "@/store/selectors/profile";
 import { useSelector } from "react-redux";
-// import { DemoUserAuth } from "@/components/Auth/DemoUserAuth";
+import { DemoUserAuth } from "@/components/Auth/DemoUserAuth";
 import { useTranslation } from "react-i18next";
 
 export default function HomeContent() {
@@ -13,7 +13,7 @@ export default function HomeContent() {
     <section className="flex grow flex-col items-center justify-center gap-4 text-center">
       <h1 className="text-xl font-black lg:text-3xl">{t("pages.home.title")}</h1>
       <p className="lg:text-xl xl:px-20">{t("pages.home.description")}</p>
-      {!profile && <div className="mt-4">{/*<DemoUserAuth />*/}</div>}
+      {!profile && <div className="mt-4">{<DemoUserAuth />}</div>}
     </section>
   );
 }
