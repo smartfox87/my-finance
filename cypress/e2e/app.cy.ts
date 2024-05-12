0db5f1cf0056484043585c5d2ce315a610080c00
Andrei Dyminski <smartfox87@gmail.com>
2024-05-12 14:59:42 +0200

    fix

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -3,38 +3,42 @@ import { DictionaryResponse } from "../support/types";
 type Page = "accounts" | "incomes" | "expenses" | "budgets" | "statistics" | "profile" | "settings";
 const pages: Page[] = ["accounts", "incomes", "expenses", "budgets", "statistics", "profile", "settings"];
 
-describe("Navigation", () => {
-  it("should navigate to the home page", () => {
-    cy.getDictionary().then(({ dictionary, lang }: DictionaryResponse) => {
-      cy.visit(`/${lang}/`);
-      cy.get('[data-cy="title"]').contains(dictionary.pages.home.title);
-      cy.get('[data-cy="description"]').contains(dictionary.pages.home.description);
-      cy.get('[data-cy="demo-auth"]').contains(dictionary.buttons.sign_in_demo_user);
+describe("Unauthorized pages", () => {
+  context("1920x1080 resolution", () => {
+    beforeEach(() => {
+      cy.viewport(1920, 1080);
     });
-  });
 
-  pages.forEach((page) => {
-    it(`should navigate to the ${page} page`, () => {
-      cy.getDictionary().then(({ dictionary }: DictionaryResponse) => {
-        cy.viewport(1920, 1080);
-        cy.visit("/"); // Заходим на главную страницу для перехода по ссылке
-        cy.get(`a[href*="/${page}"]`).first().click();
-        cy.url().should("include", `/${page}`); // Проверяем, что URL содержит /{page}
-        cy.get('[data-cy="title"]').contains(dictionary.pages[page].title);
-        cy.get('[data-cy="description"]').contains(dictionary.pages[page].description);
+    it("should navigate to the home page", () => {
+      cy.getDictionary().then(({ dictionary, lang }: DictionaryResponse) => {
+        cy.visit(`/${lang}/`);
+        cy.get('[data-cy="title"]').contains(dictionary.pages.home.title);
+        cy.get('[data-cy="description"]').contains(dictionary.pages.home.description);
         cy.get('[data-cy="demo-auth"]').contains(dictionary.buttons.sign_in_demo_user);
       });
     });
-  });
 
-  it("should navigate to the contact page", () => {
-    cy.getDictionary().then(({ dictionary }: DictionaryResponse) => {
-      cy.viewport(1920, 1080);
-      cy.visit("/");
-      cy.get('a[href*="/contact"]').first().click();
-      cy.url().should("include", "/contact");
-      cy.get('[data-cy="title"]').contains(dictionary.pages.contact.title);
-      cy.get('[data-cy="description"]').contains(dictionary.pages.contact.description);
+    pages.forEach((page) => {
+      it(`should navigate to the ${page} page`, () => {
+        cy.getDictionary().then(({ dictionary }: DictionaryResponse) => {
+          cy.visit("/"); // Заходим на главную страницу для перехода по ссылке
+          cy.get(`a[href*="/${page}"]`).first().click();
+          cy.url().should("include", `/${page}`); // Проверяем, что URL содержит /{page}
+          cy.get('[data-cy="title"]').contains(dictionary.pages[page].title);
+          cy.get('[data-cy="description"]').contains(dictionary.pages[page].description);
+          cy.get('[data-cy="demo-auth"]').contains(dictionary.buttons.sign_in_demo_user);
+        });
+      });
+    });
+
+    it("should navigate to the contact page", () => {
+      cy.getDictionary().then(({ dictionary }: DictionaryResponse) => {
+        cy.visit("/");
+        cy.get('a[href*="/contact"]').first().click();
+        cy.url().should("include", "/contact");
+        cy.get('[data-cy="title"]').contains(dictionary.pages.contact.title);
+        cy.get('[data-cy="description"]').contains(dictionary.pages.contact.description);
+      });
     });
   });
 });
