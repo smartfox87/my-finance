2f1121e4bbfd597f826bc38c92f4ceb8b299cb2f
Andrei Dyminski <smartfox87@gmail.com>
2024-06-11 19:47:26 +0200

    updates

--- cypress/support/utils.ts
+++ cypress/support/utils.ts
@@ -1,13 +1,16 @@
 import type { FilteredMultiPropsItems, FilteredSinglePropItems, FilterPropValues, SortItems, SortOrder, SortProp } from "./types";
 
+const getFloatValue = (value: string): number => parseFloat(value.replace(/\s/g, "").replace(",", "."));
+
 export const sortItems = ({ items, prop, order }: { items: SortItems; prop: SortProp; order: SortOrder }): SortItems => {
   return items.slice().sort((a, b) => {
+    console.log(prop, order);
     const [first, second] = order === "asc" ? [a, b] : [b, a];
     if (Array.isArray(first) && Array.isArray(second) && prop === "date") {
       const difference = first[0].localeCompare(second[0]);
       return difference === 0 ? first[1].localeCompare(second[1]) : difference;
     } else if (typeof first === "string" && typeof second === "string") {
-      if (prop === "amount") return parseFloat(first.replace(/\s/g, "").replace(",", ".")) - parseFloat(second.replace(/\s/g, "").replace(",", "."));
+      if (prop === "amount") return getFloatValue(first) - getFloatValue(second);
       else if (prop === "name") return first.toLowerCase().localeCompare(second.toLowerCase());
     }
     return 0;
