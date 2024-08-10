ace1c356f599f644364e33366360e3b2a6544a4b
Andrei Dyminski <smartfox87@gmail.com>
2024-08-10 12:48:56 +0200

    fix linter

--- src/predicates/budget.ts
+++ src/predicates/budget.ts
@@ -1,5 +1,6 @@
 import { FieldIds } from "@/types/field";
 import { BudgetItemData } from "@/types/budgets";
+import { isObject } from "@/predicates/common";
 
-export const isBudgetItemData = (data: Record<string, any>): data is BudgetItemData =>
-  FieldIds.NAME in data && FieldIds.AMOUNT in data && FieldIds.PERIOD in data && FieldIds.ACCOUNTS in data && FieldIds.CATEGORIES in data;
+export const isBudgetItemData = (data: unknown): data is BudgetItemData =>
+  isObject(data) && FieldIds.NAME in data && FieldIds.AMOUNT in data && FieldIds.PERIOD in data && FieldIds.ACCOUNTS in data && FieldIds.CATEGORIES in data;
