27c47c7e0901fbbe66921c8e627f6e9e82a31345
Andrei Dyminski <smartfox87@gmail.com>
2024-08-28 17:57:40 +0200

    test log

--- src/lib/api-client.ts
+++ src/lib/api-client.ts
@@ -1,6 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 import { API_KEY, API_URL } from "@/constants/config";
 
+console.log("88888888888888888888888888888888888888", API_URL, API_KEY, process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASEKEY);
+
 if (!API_URL || !API_KEY) throw new Error("API URL and Key must be provided.");
 
 export const apiClient = createClient(API_URL, API_KEY, {
