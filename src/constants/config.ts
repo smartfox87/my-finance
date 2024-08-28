d4f98d4f7ee26624d1df0e088357f4e8abbe5f9d
Andrei Dyminski <smartfox87@gmail.com>
2024-08-28 18:03:10 +0200

    test log

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,4 +1,4 @@
 export const API_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
-export const API_KEY = process.env.NEXT_PUBLIC_SUPABASEKEY;
+export const API_KEY = process.env.NEXT_PUBLIC_SUPABASE_KEY;
 
 export const RECAPTCHA_SITE_KEY = process.env.NEXT_PUBLIC_GOOGLE_RECAPTCHA_SITE_KEY;
--- src/lib/api-client.ts
+++ src/lib/api-client.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 import { API_KEY, API_URL } from "@/constants/config";
 
-console.log("88888888888888888888888888888888888888", API_URL, API_KEY, process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASEKEY);
+console.log("88888888888888888888888888888888888888", API_URL, API_KEY, process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_KEY);
 
 if (!API_URL || !API_KEY) throw new Error("API URL and Key must be provided.");
 
