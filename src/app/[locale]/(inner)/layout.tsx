26443f68d2f2581fe5c3c18f4b5d667b76241bbc
Andrei Dyminski <smartfox87@gmail.com>
2024-08-31 12:13:44 +0200

    fix layout

--- src/app/[locale]/(inner)/layout.tsx
+++ src/app/[locale]/(inner)/layout.tsx
@@ -3,57 +3,11 @@
 import { Header } from "@/components/layout/header/Header";
 import { MainNav } from "@/components/layout/navigation/MainNav";
 import { useViewport } from "@/hooks/viewport";
-import { selectUser } from "@/store/selectors/auth";
-import { useTranslation } from "react-i18next";
-import { getUserId } from "@/helpers/localStorage";
 import { MobileNav } from "@/components/layout/navigation/MobileNav";
-import { useEffect } from "react";
-import { useAppDispatch, useAppSelector } from "@/hooks/store";
 import type { ComponentChildrenProps } from "@/types/common";
-import { setCSSVariables } from "@/helpers/cssVariables";
 
 export default function MainLayout({ children }: ComponentChildrenProps) {
-  const dispatch = useAppDispatch();
   const { isTablet } = useViewport();
-  const {
-    i18n: { language },
-  } = useTranslation();
-
-  const user = useAppSelector(selectUser);
-
-  const initProfile = async (): Promise<void> => {
-    if (getUserId()) import("@/helpers/auth").then(({ handleAuthStateChange }) => handleAuthStateChange());
-    if (getUserId() && !user) {
-      const { getUserSessionThunk } = await import("@/store/slices/authSlice");
-      await dispatch(getUserSessionThunk());
-    }
-    if (!user) return;
-    const [{ getAccountsListThunk }, { getProfileThunk }, { getCurrenciesThunk }] = await Promise.all([
-      import("@/features/accounts"),
-      import("@/features/profile/store/profileSlice"),
-      import("@/store/slices/referencesSlice"),
-    ]);
-    await Promise.all([dispatch(getCurrenciesThunk()), dispatch(getProfileThunk()), dispatch(getAccountsListThunk())]);
-  };
-
-  useEffect((): void => {
-    initProfile();
-  }, [user]);
-
-  const initReferences = async (): Promise<void> => {
-    if (!user) return;
-    const { getAccountTypesThunk, getCostCategoriesThunk, getIncomeCategoriesThunk } = await import("@/store/slices/referencesSlice");
-    await Promise.all([dispatch(getAccountTypesThunk()), dispatch(getCostCategoriesThunk()), dispatch(getIncomeCategoriesThunk())]);
-  };
-
-  useEffect((): void => {
-    initReferences();
-  }, [language, user]);
-
-  useEffect((): void => {
-    setCSSVariables();
-  }, []);
-
   return (
     <>
       <Header />
