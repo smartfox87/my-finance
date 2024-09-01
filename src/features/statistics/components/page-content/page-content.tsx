f976aaa9268e9fd07514e4904f78325d8fa96673
Andrei Dyminski <smartfox87@gmail.com>
2024-09-01 11:49:52 +0200

    rename folder

--- src/features/statistics/components/page-content/page-content.tsx
+++ src/features/statistics/components/page-content/page-content.tsx
@@ -1,5 +1,5 @@
 import { Total } from "../total";
-import { ExpensesIncomes } from "../expenses-Incomes";
+import { ExpensesIncomes } from "../expenses-incomes";
 import { ExpensesBudgets } from "../expenses-budgets";
 import { ExpensesCategories } from "../expenses-categories";
 import { IncomesCategories } from "../incomes-categories";
