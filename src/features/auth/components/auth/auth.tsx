46c2736d600eaba3948ada5ca61854480cf4239f
Andrei Dyminski <smartfox87@gmail.com>
2024-09-04 19:56:45 +0200

    test

--- src/features/auth/components/auth/auth.tsx
+++ src/features/auth/components/auth/auth.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import { SignUp } from "../sign-up";
-import { SignIn } from "../sign-in";
+// import { SignUp } from "../sign-up";
+// import { SignIn } from "../sign-in";
 
 export const Auth = () => {
   return (
     <>
-      <SignUp />
-      <SignIn />
+      {/*<SignUp />*/}
+      {/*<SignIn />*/}
     </>
   );
 };
