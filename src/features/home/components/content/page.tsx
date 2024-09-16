ae120b4112d24530d36f87d3adae211e3aa76251
Andrei Dyminski <smartfox87@gmail.com>
2024-09-16 20:34:42 +0200

    updates

--- src/features/home/components/content/page.tsx
+++ src/features/home/components/content/page.tsx
@@ -1,23 +1,23 @@
 "use client";
 
-// import { selectProfile } from "@/features/profile";
-// import { DemoUserAuth } from "@/components/demo-user-auth";
-// import { useTranslation } from "react-i18next";
-// import { useAppSelector } from "@/hooks/store";
+import { selectProfile } from "@/features/profile";
+import { DemoUserAuth } from "@/components/demo-user-auth";
+import { useTranslation } from "react-i18next";
+import { useAppSelector } from "@/hooks/store";
 
 export function Page() {
-  // const { t } = useTranslation();
-  // const profile = useAppSelector(selectProfile);
+  const { t } = useTranslation();
+  const profile = useAppSelector(selectProfile);
 
   return (
     <section className="flex grow flex-col items-center justify-center gap-4 text-center">
-      {/*<h1 className="text-xl font-black lg:text-3xl" data-cy="title">*/}
-      {/*  {t("pages.home.title")}*/}
-      {/*</h1>*/}
-      {/*<p className="lg:text-xl xl:px-20" data-cy="description">*/}
-      {/*  {t("pages.home.description")}*/}
-      {/*</p>*/}
-      {/*{!profile && <div className="mt-4">{<DemoUserAuth />}</div>}*/}
+      <h1 className="text-xl font-black lg:text-3xl" data-cy="title">
+        {t("pages.home.title")}
+      </h1>
+      <p className="lg:text-xl xl:px-20" data-cy="description">
+        {t("pages.home.description")}
+      </p>
+      {!profile && <div className="mt-4">{<DemoUserAuth />}</div>}
     </section>
   );
 }
