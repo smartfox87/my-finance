493f240aff1d171f90cb6e65628599cdc9a08bb8
Andrei Dyminski <smartfox87@gmail.com>
2024-10-27 21:29:35 +0100

    add precache vercel pages

--- scripts/urls.ts
+++ scripts/urls.ts
@@ -3,6 +3,8 @@ import { writeFileSync } from "fs";
 import { PRODUCTION_URL, LOCALES, PAGES } from "../src/constants/config";
 import { Locales } from "../src/types/locales";
 
+console.log(PRODUCTION_URL, process.env.NEXT_PUBLIC_PRODUCTION_URL);
+
 const urls = PAGES.reduce<string[]>((acc, url) => {
   return acc.concat(LOCALES.map((lang) => `${PRODUCTION_URL}${lang === Locales.EN ? "" : `/${lang}`}/${url}`));
 }, []);
