db1cf23f2d6d56b309016964b7eacc56bcbc0eac
Andrei Dyminski <smartfox87@gmail.com>
2024-10-27 21:43:23 +0100

    update workflows

--- scripts/urls.ts
+++ scripts/urls.ts
@@ -3,12 +3,11 @@ import { writeFileSync } from "fs";
 import { PRODUCTION_URL, LOCALES, PAGES } from "../src/constants/config";
 import { Locales } from "../src/types/locales";
 
-console.log(PRODUCTION_URL, process.env.NEXT_PUBLIC_PRODUCTION_URL);
-
 const urls = PAGES.reduce<string[]>((acc, url) => {
   return acc.concat(LOCALES.map((lang) => `${PRODUCTION_URL}${lang === Locales.EN ? "" : `/${lang}`}/${url}`));
 }, []);
 
+console.log("11111111111111111111111111111111", PRODUCTION_URL, process.env.NEXT_PUBLIC_PRODUCTION_URL);
 console.log(urls);
 
 writeFileSync("urls.txt", urls.join("\n"), "utf8");
