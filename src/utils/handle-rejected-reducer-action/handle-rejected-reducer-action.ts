35031e29c3cca3e28c7a83e13b40b045546d89cf
Andrei Dyminski <smartfox87@gmail.com>
2024-10-27 09:54:46 +0100

    improve handle reject reducer action

--- src/utils/handle-rejected-reducer-action/handle-rejected-reducer-action.ts
+++ src/utils/handle-rejected-reducer-action/handle-rejected-reducer-action.ts
@@ -1,3 +1,5 @@
+import { IS_PRODUCTION } from "@/constants/config";
+import { captureException } from "@sentry/nextjs";
 import { showErrorMessage } from "@/utils/show-error-message";
 import type { SerializedError } from "@reduxjs/toolkit";
 import type { AccountsSliceState } from "@/types/accounts";
@@ -12,6 +14,7 @@ import type { CostsSliceState } from "@/features/expenses";
 type State = IncomesSliceState | AccountsSliceState | BudgetsSliceState | AuthSliceState | ProfileSliceState | ReferencesSliceState | CostsSliceState | StatisticsSliceState;
 
 export const handleRejectedReducerAction = (state: State, { payload, error }: { payload?: string; error: SerializedError }) => {
+  if (IS_PRODUCTION) captureException(error);
   const errorText = payload || error.message;
   if (errorText) showErrorMessage(errorText, 8);
 };
